bd225b7ec6ec3d96a74dfc0c5c95f6bfc86e75b0
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-11 18:58:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
-from application.assistant.engine import AssistantEngine
-from application.chat.engine import ChatEngine
-from application.llm.ollama_client import OllamaClient
-
 from cli.chat.cli import ChatCLI
 
+from application.bootstrap import create_assistant
+
 from dotenv import find_dotenv, load_dotenv
+
 
 load_dotenv(
     find_dotenv(),
     verbose=True,
 )
 
+
 def main():
-    llm_client = OllamaClient()
 
-
-    chat_engine = ChatEngine(
-        llm=llm_client,
-    )
-
-    assistant_engine = AssistantEngine(
-        chat_engine=chat_engine,
-    )
+    assistant = create_assistant()
 
     cli = ChatCLI(
-        engine=assistant_engine,
+        engine=assistant,
     )
 
     cli.run()
+
 
 if __name__ == "__main__":
     main()

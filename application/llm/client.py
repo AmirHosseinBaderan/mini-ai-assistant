778b20d41fa6f0186e98578902930be3960241fe
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-11 18:03:39 +0330

    Update client.py

--- application/llm/client.py
+++ application/llm/client.py
@@ -1,23 +1,24 @@
 from abc import ABC, abstractmethod
 from typing import Iterator
 
+from application.llm.message import LLMMessage
 from application.llm.response import LLMResponse
-
+from application.llm.tool import LLMTool
 
 class LLMClient(ABC):
 
     @abstractmethod
     def stream(
         self,
-        messages: list[dict[str, str]],
+        messages: list[LLMMessage],
     ) -> Iterator[str]:
         pass
 
     @abstractmethod
     def chat(
         self,
-        messages: list[dict[str, str]],
-        tools: list[dict],
+        messages: list[LLMMessage],
+        tools: list[LLMTool],
     ) -> LLMResponse:
         pass
 

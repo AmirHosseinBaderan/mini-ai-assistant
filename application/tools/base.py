c969b58ba3fe82824beea5a271628cbc8ece4df1
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-11 15:17:19 +0330

    create : tool base

---
+++ application/tools/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Tool(ABC):
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        ...
+
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        ...
+
+    @abstractmethod
+    def execute(
+        self,
+        **kwargs: Any,
+    ) -> Any:
+        ...

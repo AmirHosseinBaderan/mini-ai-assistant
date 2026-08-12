afb6ac962b5aa125614452b16b87d908a1f4fed5
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-12 12:53:09 +0330

    update : add async support

--- application/assistant/engine.py
+++ application/assistant/engine.py
@@ -1,4 +1,4 @@
-from collections.abc import Iterator
+from collections.abc import Iterator, AsyncIterator
 
 from application.agent.agent import Agent
 from application.chat.history import ConversationHistory
@@ -32,4 +32,35 @@ class AssistantEngine:
 
         self.history.add_assistant(
             "".join(response)
+        )
+
+    async def astream(
+            self,
+            text: str,
+    ) -> AsyncIterator[str]:
+
+        messages = self.history.get_messages()
+
+        messages.append(
+            LLMMessage(
+                role="user",
+                content=text,
+            )
+        )
+
+        self.history.add_user(
+            content=text
+        )
+
+        assistant_chunks = []
+
+        async for chunk in self.agent.astream(
+                messages
+        ):
+            assistant_chunks.append(chunk)
+
+            yield chunk
+
+        self.history.add_assistant(
+            content="".join(assistant_chunks)
         )

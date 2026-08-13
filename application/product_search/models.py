b22a9b44c14cc03721d77a0d220cc22282cc72e5
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-13 19:47:54 +0330

    Update models.py

--- application/product_search/models.py
+++ application/product_search/models.py
@@ -1,6 +1,15 @@
 from dataclasses import dataclass
 
+
 @dataclass(frozen=True)
 class SiteConfig:
-    name:str
-    search_url:str
+    name: str
+    search_url: str
+
+
+@dataclass(frozen=True)
+class Product:
+    name: str
+    price: str | None
+    url: str
+    source: str

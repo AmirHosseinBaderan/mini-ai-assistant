0db5259e76ad01a6993c33717d38b3c153bc0605
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-14 00:28:43 +0330

    update : change directory structure

--- application/mcp/__init__.py
+++
@@ -1,4 +0,0 @@
-from .server.tools.product_search import ProductSearchTool
-from .server.server import  mcp
-from .client.client import MCPClient
-from .tools import discover_tools,discover_tools_from_clients

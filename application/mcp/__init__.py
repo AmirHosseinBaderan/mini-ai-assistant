e3ba255ebf634e8708529ea5b975a6c6cafc898b
amir hossin <65053996+AmirHosseinBaderan@users.noreply.github.com>
2026-08-13 23:16:23 +0330

    Update __init__.py

--- application/mcp/__init__.py
+++ application/mcp/__init__.py
@@ -1,4 +1,4 @@
-from .server.tools.product_search import search_products
+from .server.tools.product_search import ProductSearchTool
 from .server.server import  mcp
 from .client.client import MCPClient
 from .tools import discover_tools,discover_tools_from_clients
